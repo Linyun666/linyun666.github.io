38974de4db613dc5a39b222a3ba4e0d3df22d124
Linyun666 <3035601172@qq.com>
2025-04-29 18:38:31 +0800

    Add files via upload

---
+++ gangwei_shouye.js
@@ -0,0 +1,15 @@
+function onClick() {
+  location.href = '../../pages/gangweixiangqingye/gangweixiangqingye.html';
+}
+
+function onClick_1() {
+  location.href = '../../pages/jianli/jianli.html';
+}
+
+function onClick_2() {
+  location.href = '../../pages/mianshi/mianshi.html';
+}
+
+function onClick_3() {
+  location.href = '../../pages/geren/geren.html';
+}
